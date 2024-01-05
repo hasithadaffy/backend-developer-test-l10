c967669269c64c073208b318ce323d9254acaae6
hasithadaffy <hasithadman@gmail.com>
2024-01-05 10:18:34 +0530

    feat(Models): added the relationship between user and the achievements and badges

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,5 +66,21 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Lesson::class)->wherePivot('watched', true);
     }
+
+    /**
+     * The achievements that belong to the user.
+     */
+    public function achievements()
+    {
+        return $this->hasMany(UserAchievement::class);
+    }
+
+    /**
+     * The badges that belong to the user.
+     */
+    public function badges()
+    {
+        return $this->hasMany(UserBadge::class);
+    }
 }
 
